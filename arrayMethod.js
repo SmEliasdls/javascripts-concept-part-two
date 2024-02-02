e3c701f11be919143d7c08dc1bca7b74e919998e
sm Elias <e01711706710@gmail.com>
2024-02-02 21:47:56 +0600

    Object

---
+++ arrayMethod.js
@@ -0,0 +1,7 @@
+const x = "My name Is Programming Hero";
+
+const words = x.split(" "); 
+console.log(words); // ["My", "name", "Is", "Programming",
+
+const y = words.join("_");
+console.log(y);
---
+++ object.js
@@ -0,0 +1,47 @@
+// const name = "Travis";
+// const age = 2:
+// const isMale = true;
+// const color = white;
+// const species = "Hulo Biral";
+
+// const myPet = {
+//     name: "Travis",
+//     age: 2,
+//     color: "white",
+//     species: "Hulo Biral",
+//     isMale: true,
+//     food: "Fish",
+//     "is married": true
+// };
+
+
+// myPet.sound = "meo meow";
+// // Dot notation
+// console.log(myPet.food); // Output: Fish
+
+// // Bracket notation
+// const catSpecies = myPet["species"];
+// console.log(catSpecies); // Output: Hulo Biral
+
+// // Bracket notation for a property with a space in its name
+// console.log(myPet["is married"]); // Output: true
+
+// delete myPet.age;
+// console.log(myPet);
+
+const myPet = {
+    name: "Travis",
+    age: 2,
+    color: "white",
+    species: "Hulo Biral",
+    isMale: true,
+    food: "Fish",
+    "is married": true
+};
+
+// object loopp for in
+for (let key in myPet) {
+    console.log(key, typeof key);
+}
+console.log("------------------------");
+
