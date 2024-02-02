3a32f1ce4184fefc3164f47a0266a1427e8f0b51
sm Elias <e01711706710@gmail.com>
2024-02-02 21:12:00 +0600

    string

---
+++ string.js
@@ -0,0 +1,22 @@
+// const x = "heLlo";
+// const y = "Hello";
+
+// if(x.toLowerCase()===y.toLowerCase()){
+//     console.log("Both Are Same");
+// }else{
+//     console.log("Difference");
+// }
+
+
+// const m = "Hello World";
+// const n = "hello";
+// console.log(m.toLowerCase().includes(n.toLowerCase()));
+
+const a = "hello";
+const b = "hello ";
+
+if( a === b.trim()){
+console.log("Both are Same");
+}else{
+    console.log("Both Are Difference");
+}
